a939b0e74d92b3c949c8b157cfa523c5fed20ae0
Mansi <mansisaraf4@gmail.com>
2020-07-06 20:51:49 +0530

    Functions

--- Practice/script.js
+++ Practice/script.js
@@ -2,7 +2,7 @@
 
 //Function Constructor always with capital
 
-var Person = function(name,yearOfBirth,job){
+/*var Person = function(name,yearOfBirth,job){
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
@@ -20,4 +20,101 @@ console.log(john);
 // john.calculateAge();
 
 var mansi = new Person("Mansi",1995,"Developer");
-mansi.calculateAge();
+mansi.calculateAge();*/
+
+/*
+
+//Object.create
+
+//First create a simple object
+
+var personProto = {
+    calculateAge: function() {
+        console.log(2020-this.yearOfBirth);
+    }
+};
+
+var gina = Object.create(personProto);
+
+
+
+///Passing function as argument
+
+var years = [1990,1995,1890,2015,2000];
+
+function arrayCalculator(arr,fn){
+    var res = [];
+    for(var i=0;i<arr.length;i++){
+        res.push(fn(arr[i]));
+    }
+    return res;
+}
+
+function ageCalc(year){
+    return 2020-year;
+}
+
+var ageArray = arrayCalculator(years,ageCalc);
+console.log(ageArray);
+
+///Marks of students
+
+var mathMarks = [30,25,45,50,13];
+function percCalc(marks){
+    return marks*100/50;
+}
+
+var perArr = arrayCalculator(mathMarks,percCalc);
+console.log(perArr);
+
+//Function returning function
+
+function ingredientsRequired(dish){
+    switch (dish){
+        case "Momos": 
+            return function(name){
+            console.log("Hi!"+name+", to make momos you will need maida, cabbage and carrot");
+            }
+
+        case "Roti":
+            return function(name){
+                console.log("Hi!"+name+", to make rotis, you will need wheat flour, water and a pinch of salt.");
+            }
+
+        default:
+            return function(name){
+                console.logs("Which dish ingredients would you like to know,"+name+" ?");
+            }
+
+    } 
+}
+
+ingredientsRequired("Roti")("Mansi");*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
