915fa40034e0e8293ac6c0fee95902508897fe0a
mansisaraf4 <mansisaraf4@gmail.com>
2020-07-15 19:59:12 +0530

    Budget app modifications

--- budgety/Budgety.js
+++ budgety/Budgety.js
@@ -1,3 +1,10 @@
+var totalIncome, totalExpense,currentIncome,currentExpense;
+currentIncome = document.querySelector('.budget__income--value').textContent;
+currentExpense = document.querySelector('.budget__expenses--value').textContent;
+totalIncome = parseFloat(currentIncome.split(" ")[1].split(",").join(""));
+totalExpense = parseFloat(currentExpense.split(" ")[1].split(",").join(""));
+
+
 // Button to add items
 document.querySelector('.add__btn').addEventListener('click',function(){
     addItem();
@@ -10,20 +17,24 @@ document.addEventListener('keypress',function(event){
 })
 
 var addItem = function(){
-    var amount,currentIncome,currentExpense,updatedIncome,updatedExpense;
-    currentIncome = document.querySelector('.budget__income--value').textContent;
-    currentExpense = document.querySelector('.budget__expenses--value').textContent;
-
     var record = getInput();
     if(record.type==="inc" && record.value>0){
-        amount = parseFloat(currentIncome.split(" ")[1].split(",").join(""))+parseFloat(record.value);
-        document.querySelector('.budget__income--value').textContent = Intl.NumberFormat('en-IN').format(amount);
-    } else if(record.type==="inc" && record.value>0){
-        amount = parseFloat(currentIncome.split(" ")[1].split(",").join(""))-parseFloat(record.value);
-        document.querySelector('.budget__expenses--value').textContent = Intl.NumberFormat('en-IN').format(amount);
+        totalIncome = totalIncome+parseFloat(record.value);
+        document.querySelector('.budget__income--value').textContent = "+ "+Intl.NumberFormat().format(totalIncome);
+    } else if(record.type==="exp" && record.value>0){
+        totalExpense = totalExpense+parseFloat(record.value);
+        document.querySelector('.budget__expenses--value').textContent = "- "+ Intl.NumberFormat().format(totalExpense);
     }
+    calcBudgetAndExpensePercent(totalIncome,totalExpense);
+
     //Code to add items in the list
     // Update the amounts
+}
+var calcBudgetAndExpensePercent = function(income,expense){
+    var budget = income - expense;
+    document.querySelector('.budget__value').textContent = Intl.NumberFormat().format(budget);
+    var expPercent = (expense/income)*100;
+    document.querySelector('.budget__expenses--percentage').textContent = Intl.NumberFormat().format(expPercent.toFixed())+"%";
 }
 
 var getInput = function(){    
