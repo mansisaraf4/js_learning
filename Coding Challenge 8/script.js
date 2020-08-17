6ca050d87a94d99573476eada3878d78d19c3842
Mansi <mansisaraf4@gmail.com>
2020-08-17 15:42:57 +0530

    Coding Challenge 8 Complete

--- Coding Challenge 8/script.js
+++ Coding Challenge 8/script.js
@@ -22,13 +22,13 @@ All the report data should be printed to the console.
 HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
 
 */
-let ageSum = 0;
 class Element{
     constructor(name,buildYear){
         this.name = name;
         this.buildYear = buildYear;
     }
 }
+//Park Class and functions
 class Park extends Element{
     constructor(name, buildYear, treeCount, parkArea){
         super(name,buildYear);
@@ -39,11 +39,6 @@ class Park extends Element{
     calcDensity(){
         console.log(`The density of ${this.name} is ${(this.treeCount/this.parkArea).toFixed(2)}`) ;
     }
-    isMoreThan1000(){
-        if(this.treeCount>1000){
-            console.log(`${this.name} has more than 1000 trees!`);
-        }
-    }
     calcAge(){
         var age = new Date().getFullYear() - this.buildYear;
         return age;
@@ -52,6 +47,37 @@ class Park extends Element{
 
     }
 }
+//Find out parks that have more than 1000 trees
+var isMoreThan1000 = function(parkArray){
+    for (let park of parkArray){
+        if(park.treeCount > 1000){
+            console.log(`${park.name} has more than 1000 trees!`);
+        }
+    }      
+}
+//Calculate Average Age of Parks
+var calcAvgAge =  function (parkArray){
+    let sum = 0;
+    for (let park of parkArray){
+        sum += park.calcAge();
+    }
+    console.log(`Average age of parks is ${(sum/parkArray.length).toFixed(2)}`);
+}
+//Display Park Details
+var displayParksDetails = function(parkArray){
+    console.log('================PARKS================');
+    for (let park of parkArray){
+        park.calcDensity();
+    }
+    calcAvgAge(parkArray);
+    isMoreThan1000(parkArray);
+}
+const parkA = new Park('ParkA',1990,500,546);
+const parkB = new Park('ParkB',1800,800,1000);
+const parkC = new Park('ParkC',1700,1200,1000);
+let parkArr = [parkA, parkB, parkC];
+displayParksDetails(parkArr);
+//Street Class and Functions
 class Street extends Element{
     constructor(name, buildYear, length, size = 'normal'){
         super(name, buildYear);
@@ -59,13 +85,28 @@ class Street extends Element{
         this.size = size;
     }
 }
-const parkA = new Park('ParkA',1990,500,546);
-const parkB = new Park('ParkB',1800,800,1000);
-const parkC = new Park('ParkC',1700,1200,1000);
-console.log('================PARKS================');
-parkA.calcDensity();
-parkA.isMoreThan1000();
-parkB.calcDensity();
-parkB.isMoreThan1000();
-parkC.calcDensity();
-parkC.isMoreThan1000();
+const streetA = new Street('StreetA', 2000, 250,'small');
+const streetB = new Street('StreetB', 1980, 1500,'huge');
+const streetC = new Street('StreetC', 2010, 500);
+let calcTotalandAvgLength = function(streetArr){
+    let avg = 0, total = 0;
+    for (let street of streetArr){
+        total +=street.length;
+    }
+    avg = (total/streetArr.length).toFixed(2);
+    // return [total, avg];
+    console.log(`Total length of the streets is ${total} km and average length of the streets is ${avg} km`);
+}
+let displaySize = function(streetArr){
+    for (let street of streetArr){
+        console.log(`The size of street ${street.name} is ${street.size}`);
+    }  
+}
+let displayStreetDetails= function(streetArr){
+    console.log('================STREET================');
+    calcTotalandAvgLength(streetArr);
+    displaySize(streetArr);
+}
+const streetD = new Street('StreetD',1900,50,'tiny');
+let streetArray = [streetA, streetB, streetC, streetD];
+displayStreetDetails(streetArray);
