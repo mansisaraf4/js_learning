b0d01d109381d484ca82b68cd00ec8e3b4704fbd
Mansi <mansisaraf4@gmail.com>
2020-08-23 21:54:10 +0530

    Forkify App In Progress

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,4 +1,14 @@
-// Global app controller
-import x from './test';
-const num = 134;
-console.log(`${x} is imported from test.js along with const ${num}`);
+// https://forkify-api.herokuapp.com/api/search?q=pizza
+import Search from './models/Search';
+/* Global state of the app
+*-- Search object
+*-- Current recipe object
+*--Shopping list object
+*--Liked recipe
+*/ 
+const state = {};
+document.querySelector('.search').addEventListener('submit', e =>{
+    e.preventDefault();
+});
+const search = new Search('pasta');
+search.getResults();
