afe3917b7857fac0c73c4eeaabc44374266554c8
Mansi <mansisaraf4@gmail.com>
2020-08-25 17:18:31 +0530

    Forkify Inprogress

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,5 +1,7 @@
 // https://forkify-api.herokuapp.com/api/search?q=pizza
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
 /* Global state of the app
 *-- Search object
 *-- Current recipe object
@@ -7,8 +9,29 @@ import Search from './models/Search';
 *--Liked recipe
 */ 
 const state = {};
-document.querySelector('.search').addEventListener('submit', e =>{
+const controlSearch = async () => {
+    // Get Query from View
+    const query = searchView.getInput(); //TODO
+    console.log(`${query} is the query`);
+    if(query) {
+        // 2. new Search Object and add to state
+        state.search = new Search(query);
+        
+        //3. Project UI for results
+
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        // 5. Render results on UI
+        console.log(state.search.result);
+
+    }
+}
+
+
+elements.searchForm.addEventListener('submit', e =>{
     e.preventDefault();
+    controlSearch();
 });
-const search = new Search('pasta');
-search.getResults();
+// const search = new Search('pasta');
+// search.getResults();
